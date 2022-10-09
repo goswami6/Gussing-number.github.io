21302f996b408801796691f246276a4856551c70
Akanksha Goswami <akankshagoswami567@gmail.com>
2022-10-09 14:23:32 +0530

    game

---
+++ main.js
@@ -0,0 +1,16 @@
+let btn = document.getElementById('btn');
+let output = document.getElementById('outputtext');
+
+let number = [Math.floor(Math.random() * 100)]
+
+btn.addEventListener('click', function(){
+    let input = document.getElementById('userInput').value;
+    if(input == number){
+        output.innerHTML = `you guessed right, your number was ${number}`
+    } else if(input < number){
+        output.innerHTML = "You guessed too low!"
+    };
+    if (input > number){
+        output.innerHTML = "You guessed too high!"
+    }
+});
